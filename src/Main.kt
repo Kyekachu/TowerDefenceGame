5e252a01941951a4f4c05c611f5a28b3320654b2
P282860 <kyrene-taaffe@hotmail.com>
2025-08-08 10:09:17 +0800

    o/

--- src/Main.kt
+++ src/Main.kt
@@ -4,5 +4,14 @@ import Enemy
 import Tower
 
 fun main() {
-
+    val e1 = Enemy()
+    val e2 = Enemy(500)
+    println("### enemy HPs before tower attack ###")
+    println("Enemy1: ${e1.hp}")
+    println("Enemy2: ${e2.hp}")
+    val tower = Tower()
+    tower.attack(e1, 10)
+    println("### enemy HPs after tower attack ###")
+    println("Enemy1: ${e1.hp}")
+    println("Enemy2: ${e2.hp}")
 }
