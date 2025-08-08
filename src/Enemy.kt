0a0227423e031eb1e7c9a1e72233bcb6b968204b
P282860 <kyrene-taaffe@hotmail.com>
2025-08-08 09:56:30 +0800

    o/

---
+++ src/Enemy.kt
@@ -0,0 +1,16 @@
+internal class Enemy {
+    var hp: Int
+        private set
+
+    constructor() {
+        hp = 100
+    }
+
+    constructor(hp: Int) {
+        this.hp = hp
+    }
+
+    fun getDamage(damage: Int) {
+        hp -= damage
+    }
+}
